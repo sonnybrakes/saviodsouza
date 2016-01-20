8f077b534367286663d5876db208a6d082acccd5
Michel Frechette <mifrec@hotmail.com>
2016-01-20 13:25:57 -0500

    treehouse website template

---
+++ jscript/rDev.js
@@ -0,0 +1,80 @@
+// var friends = ["Nick", "Michael", "Amit", "Debbie Roy", "John"];
+
+// console.log(friends);
+
+// console.log(friends.length);
+
+// var friendNumber = 1
+// console.log(friends[friendNumber]);
+
+// for (var i = 0; i < friends.length; i+=1) {
+// 	console.log(friends[i]);
+// };
+
+var me = {
+	first_name: "Michel", last_name: "Frechette"
+};
+
+console.log(me.first_name);
+console.log(me.last_name);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log("hello");
+
+// var name = prompt("what is your name?");
+
+// alert("hello " + name);
+
+// console.log("the user's name is " + name);
+
+
+// console.log("before");
+
+// var name = prompt("What is your name?");
+
+// if (name) {}
+// alert("Hello, " + name);
+
+// var name = prompt("What is your name?");
+
+// if (name) {
+// 	console.log("if block");
+// } else {
+// 	console.log("else block");
+// }
+
+// var counter = 10;
+
+// while (counter) {
+// 	console.log("Hello, World!");
+// 	counter = counter - 1;
+// }
+
+// while (prompt("What is your name?")) {
+// 	console.log ("Got your name")
+// }
+
+
+// for (var counter = 10; counter; counter = counter -1) {
+// 	console.log("Hello World!", counter)
+// }
+
+// console.log("after");
